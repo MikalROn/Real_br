dcf484a930149e18f278c6a547d43551d1c1b75b
Khijad_hiliad <heromon.9010@gmail.com>
2022-09-26 09:43:12 -0300

    reformatando arquivo

--- tests/test_real_br.py
+++ tests/test_real_br.py
@@ -1,76 +1,77 @@
 import pytest
 from real_br import *
 
+
 class Test:
-    
+
     def test_se_real_br_aceita_numeros_inteiros(self):
         entrada = 100
         esperado = 'R$ 100,00'
-        
+
         entrada = Real(entrada)
         entrada = str(entrada)
-        
+
         assert entrada == esperado
-        
+
     def test_se_real_br_aceita_numeros_float(self):
         entrada = 100.00
         esperado = 'R$ 100,00'
-        
+
         entrada = Real(entrada)
         entrada = str(entrada)
-        
+
         assert entrada == esperado
-        
+
     def test_se_real_br_aceita_numeros_em_str_sem_virgula_ou_ponto(self):
         entrada = '100'
         esperado = 'R$ 100,00'
-        
+
         entrada = Real(entrada)
         entrada = str(entrada)
-        
+
         assert entrada == esperado
-        
+
     def test_se_real_br_aceita_numeros_em_str_com_virgula_e_uma_casa_decimal(self):
         entrada = '100,0'
         esperado = 'R$ 100,00'
-        
+
         entrada = Real(entrada)
         entrada = str(entrada)
-        
+
         assert entrada == esperado
-        
+
     def test_se_real_br_aceita_numeros_em_str_com_virgula_e_duas_casas_decimais(self):
         entrada = '100,00'
         esperado = 'R$ 100,00'
-        
+
         entrada = Real(entrada)
         entrada = str(entrada)
-        
+
         assert entrada == esperado
-        
+
     def test_se_real_br_aceita_numeros_em_str_com_ponto_e_duas_casas_decimais(self):
         entrada = '100.00'
         esperado = 'R$ 100,00'
-        
+
         entrada = Real(entrada)
         entrada = str(entrada)
-        
+
         assert entrada == esperado
-        
+
     def test_se_real_br_aceita_numeros_em_str_com_ponto_e_uma_casa_decimal(self):
         entrada = '100.0'
         esperado = 'R$ 100,00'
-        
+
         entrada = Real(entrada)
         entrada = str(entrada)
-        
+
         assert entrada == esperado
-        
+
     def test_se_real_br_aceita_numeros_em_str_com_ponto_e_sem_casa_decimal(self):
         entrada = '100'
         esperado = 'R$ 100,00'
-        
+
         entrada = Real(entrada)
         entrada = str(entrada)
-        
+
         assert entrada == esperado
